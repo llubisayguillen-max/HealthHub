9f0df8969608a449f88f3228e04902a90fee55de
llubisayguillen-max <llubisay.guillen@davinci.edu.ar>
2025-09-18 15:25:23 -0300

    prueba

---
+++ src/HistorialMedico.java
@@ -0,0 +1,61 @@
+import java.util.Date;
+
+public class HistorialMedico {
+	
+	private int id;
+	private Date fechaCreacion;
+	private String observaciones;
+	private String[] archivos;
+
+	public HistorialMedico(int id, Date fechaCreacion, String observaciones, String[] archivos) {
+		this.id = id;
+		this.fechaCreacion = fechaCreacion;
+		this.observaciones = observaciones;
+		this.archivos = archivos;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public String[] getArchivos() {
+		return archivos;
+	}
+
+	public void setArchivos(String[] archivos) {
+		this.archivos = archivos;
+	}
+
+	public void agregarHistorial() {
+		System.out.println("Agregando historial médico.");
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Historial Médico: \nId=" + id + "\nFecha de creación=" + fechaCreacion + "\nObservaciones="
+				+ observaciones;
+	}
+}
+
