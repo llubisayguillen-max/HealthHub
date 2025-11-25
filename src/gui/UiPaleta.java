dc2eea1bca4c5c5a085c01e657e284d98ea1d655
Eliibermudez <eliana.bermudez@davinci.edu.ar>
2025-11-25 00:20:14 -0300

    Update - Frame MenuMedico (#23)

---
+++ src/gui/UiPaleta.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.awt.Color;
+
+public class UiPaleta {
+
+    // Azul principal
+    public static final Color COLOR_PRIMARY     = Color.decode("#006699");
+    // Fondo general
+    public static final Color COLOR_BACKGROUND  = Color.decode("#EEEEEE");
+    // Tarjetas
+    public static final Color COLOR_CARD_BG     = Color.WHITE;
+    public static final Color COLOR_CARD_BORDER = Color.decode("#CCCCCC");
+
+    // Acentos
+    public static final Color COLOR_ACCENT      = Color.decode("#6fc9c0");
+
+    // Verde menta oscuro (el que elegimos para títulos)
+    public static final Color MINT_DARK         = Color.decode("#3FA38E");
+
+    // Peligro / acciones destructivas
+    public static final Color COLOR_DANGER      = Color.decode("#FF4444");
+
+    // Texto secundario
+    public static final Color COLOR_TEXT_MUTED  = new Color(105, 105, 105);
+}
